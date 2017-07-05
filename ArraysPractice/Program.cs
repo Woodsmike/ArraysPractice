adeaa073bef364ba80c64b4c902c71c6c672df07
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-05 11:56:36 -0400

    Further practice problems

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -11,42 +11,61 @@ namespace ArraysPractice
         static void Main(string[] args)
         {
             //declaring an array without initializing
-            int[] myFirstArray;
+            //int[] myFirstArray;
+
             //declare and intialize an array
-            string[] myStringArray = { "First", "Second", "Third", "Fourth" };
+            //string[] myStringArray = { "First", "Second", "Third", "Fourth" };
+
             //declare and initialize a char array with the letters of my last name
-            char[] myCharArray = { 'W', 'o', 'o', 'd', 's' };
+            //char[] myCharArray = { 'W', 'o', 'o', 'd', 's' };
             //Print to console 3 elements of my display
-            Console.WriteLine(myCharArray[0]);
-            Console.WriteLine(myCharArray[2]);
-            Console.WriteLine(myCharArray[4]);
+            //Console.WriteLine(myCharArray[0]);
+            //Console.WriteLine(myCharArray[2]);
+            //Console.WriteLine(myCharArray[4]);
             //Create the following arrays and print using the correct index numbers.
+
             //Array of the top 10 vacation spots
             //print the first and last vacation spots
-            string[] vacationSpots = {"Myrtle Beach","Hawaii","Las Vegas","San Diego","Tokyo",
-                "Cozumel","Jamaica","Paris","Mexico","Chile"};
-            Console.WriteLine(vacationSpots[0]);
-            Console.WriteLine(vacationSpots[9]);
+            //string[] vacationSpots = {"Myrtle Beach","Hawaii","Las Vegas","San Diego","Tokyo",
+            //    "Cozumel","Jamaica","Paris","Mexico","Chile"};
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
+
             //Array or the ages of 3 children you know
             //print each child's age
-            int[] childAge = { 14, 16, 18 };
-            Console.WriteLine(childAge[0]);
-            Console.WriteLine(childAge[1]);
-            Console.WriteLine(childAge[2]);
+            //int[] childAge = { 14, 16, 18 };
+            //Console.WriteLine(childAge[0]);
+            //Console.WriteLine(childAge[1]);
+            //Console.WriteLine(childAge[2]);
+
             //Array of GPA's on a 4.0 scale of 8 students - you don't have to print
-            double[] studentGPAs = { 3.0d, 3.4d, 2.8d, 5.0d, 3.6d, 1.5d, 3, 9d, 3.3d };
+            //double[] studentGPAs = { 3.0d, 3.4d, 2.8d, 5.0d, 3.6d, 1.5d, 3, 9d, 3.3d };
+
             //Array of the First Letter of 4 of your classmate's names who are sitting 
             //near you
             //print all four on the same line
-            char[] firstLetter = { 'H', 'S', 'I', 'P' };
-            Console.Write(firstLetter[0]);
-            Console.Write(firstLetter[1]);
-            Console.Write(firstLetter[2]);
-            Console.Write(firstLetter[3]);
-            Console.WriteLine();
-            Console.WriteLine("{ 0},{ 1},{ 2},{ 3}",firstLetter[0],firstLetter[1],firstLetter[2],firstLetter[3] );
+            //char[] firstLetter = { 'H', 'S', 'I', 'P' };
+            //Console.Write(firstLetter[0]);
+            //Console.Write(firstLetter[1]);
+            //Console.Write(firstLetter[2]);
+            //Console.Write(firstLetter[3]);
+            //Console.WriteLine();
+            //Console.WriteLine("{ 0},{ 1},{ 2},{ 3}", firstLetter[0], firstLetter[1], firstLetter[2], firstLetter[3]);
 
-            
+            //data type[] nameOfArray = new data type[number of places held] 
+            //int[] numberOfSeats = new int[21];
+            //string[] studentsInClass = new string[21];
+            //studentsInClass[0] = "Jordan";
+
+            //Console.WriteLine("Please enter the name of the next student.");
+            //studentsInClass[1] = Console.ReadLine();
+            //Console.WriteLine("The student in the second seat is {0}.", studentsInClass[1]);
+
+            //Console.WriteLine("Please enter the age of the person in the first seat.");
+            //int[] ageOfStudentsInClass = new int[21];
+            //ageOfStudentsInClass[0] = int.Parse(Console.ReadLine());
+            //Console.WriteLine("The age of the student in the first seat is " + ageOfStudentsInClass[0] +".");
+
         }
     }
 }
